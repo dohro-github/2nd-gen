12ed68cb1baac7cee4e98bea08c74f242d2d895a
Thefunguy-Github <Dylanadion@outlook.com>
2024-08-28 06:26:11 -0700

    Just another update

--- Assets/Code/top_bar.gd
+++ Assets/Code/top_bar.gd
@@ -1,18 +1,17 @@
 extends Panel
 
-@onready var nav_drawer = $"Nav-drawer"
+@onready var settings_menu = $settings
 
 var nav_drawer_shown = false
 
-func _ready() -> void:
-	if nav_drawer:
-		nav_drawer.hide()
+# "This might actually be a step sequencer, not a tracker" -Quellusdev 8/27/24
+# "Its a tracker" -Quellusdev 8/27/24
 
-func _on_navbutton_pressed() -> void:
-	if nav_drawer:
-		if nav_drawer_shown == false:
-			nav_drawer.show()
-			nav_drawer_shown = true
-		elif nav_drawer_shown == true:
-			nav_drawer.hide()
-			nav_drawer_shown = false
+func _ready():
+	settings_menu.hide()
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+func _on_settings_pressed() -> void:
+	settings_menu.show()
